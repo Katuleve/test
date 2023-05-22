f4a6d596ee58a35b11d4f284678095cb0a519686
Katuleve <112998094+Katuleve@users.noreply.github.com>
2023-05-22 09:47:06 +0300

    added tit.py file

---
+++ tit.py
@@ -0,0 +1,10 @@
+from transformers import pipeline
+
+generator = pipeline('text-generation', model='distilgpt2')
+
+res = generator(
+    'IN this course we will teach you how to',
+    max_legth = 30,
+    num_return_sequences = 2)
+
+print(res)
